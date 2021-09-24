d6fff3caa177fd66ec23229258c2d71954490e44
caiocastrofrs <caiocastrofrs@gmail.com>
2021-09-24 18:08:03 -0300

    update: recuperando os dados da API

--- js/app.js
+++ js/app.js
@@ -1,5 +1,36 @@
-let btn = document.getElementById('btn');
-
-btn.addEventListener('click',function() {
-    document.getElementById('modal').classList.toggle('modal-ativo');
+let btn = document.getElementById('btn-criar');
+let containerCard = document.querySelector('.container-card');
+btn.addEventListener("click", (e) => {
+    e.preventDefault();
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+    fetch(`https://jsonplaceholder.typicode.com/todos/`, options)
+        .then(response => {
+            response.json()
+            .then (dados => {
+                for(dado of dados) {
+                    console.log(dado.id);
+                    let card = `
+                    <div class="todo-card">
+                        <div class="header-card">
+                            <span>${dado.id}</span>
+                            <h3>${dado.completed}</h3>
+                            <span>excluir</span>
+                        </div>
+                        <p class="main-card">
+                        ${dado.title}
+                        </p>
+                        <div class="footer-card">
+                            <div><h4>CRIADO EM</h4><span>01/01/21</span></div>
+                            <div><h4>DATA LIMITE</h4><span>05/01/21</span></div>
+                        </div>
+                    </div>
+                    `
+                    containerCard.innerHTML += card;
+                }
+            })
+        })
 })
